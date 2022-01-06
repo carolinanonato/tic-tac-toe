cbbeda1b94f0b931f409788ebe9ec5cfb685e0d8
Carolina Nonato <csnonato@gmail.com>
2022-01-06 10:52:11 -0500

    board

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header'
-
+import Board from './objects/Board'
 
 
 function App() {
   return (
     <>
       <Header />
+      <Board />
     </>
   );
 }
---
+++ src/objects/Board/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css'
+
+const Board = () => {
+   return (
+     <article className='board'></article>
+   )
+}
+
+export default Board;
