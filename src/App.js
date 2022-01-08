b30de3e0d99c93e36f233d7a0c9ed9d713e39bdd
Carolina Nonato <csnonato@gmail.com>
2022-01-08 14:18:42 -0500

    new checkbox is checking

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header'
-import Label from './objects/Label';
-import Input from './objects/Input';
+import Checkbox from './objects/Checkbox';
+
 
 
 
@@ -11,7 +11,7 @@ const App = () => (
     <main className='app'>
       <Header />
       <Hashtag />
-      <Input  id="show"  value="show" content="Show events"/>
+      <Checkbox  id="show"  value="show" type="checkbox" content="Show events"/>
      
      
     </main>
