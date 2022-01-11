d703a4c5440352d6b82774cc48e4ba62072fdc05
Carolina Nonato <csnonato@gmail.com>
2022-01-11 15:52:32 -0500

    Refactoring Menu to MenuGame

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Logo from '../../objects/Logo'
 import About from '../../objects/About'
-import Menu from '../../objects/Menu'
+import MenuGame from '../../objects/MenuGame'
 import './style.css'
 
 const Header = ({onClick}) => (
     <header className="header">
         <Logo />
         <About onClick={onClick}/>
-        <Menu onClick={onClick}/>
+        <MenuGame onClick={onClick}/>
 
     </header>
 )
--- src/objects/Menu/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style.css'
-
-
-const Menu = ({onClick}) => (<a href="#menu" 
-className='burger' onClick={onClick}><span className='center'>Menu</span></a>);
-
-export default Menu;
