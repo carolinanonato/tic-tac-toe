7a37a5862f8c77f91ee43643f5a8b13605ca0903
Carolina Nonato <csnonato@gmail.com>
2022-01-12 17:52:36 -0500

    added player-game click event

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -3,13 +3,17 @@ import './style.css'
 import playerX from '../../img/playerx.png'
 import playerO from '../../img/playero.png'
 
+const handleClick = () => {
+    console.log('vamo que vamo')
+}
+
 const PlayerGame = ({ player = false}) => {
    const players = [];
    players['x'] = playerX;
    players['o'] = playerO;
 
     return (
-    <button className="player-game">
+    <button onClick={handleClick} className="player-game">
          {player && <img src={players[player]} alt={`Player ${player.toUpperCase()}`} />}
     </button>
     )
