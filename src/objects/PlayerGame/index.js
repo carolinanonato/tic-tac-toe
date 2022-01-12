5298204d4fdc553ce083af4a8f3f6889e511726b
Carolina Nonato <csnonato@gmail.com>
2022-01-12 16:40:35 -0500

    empty player game

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -3,15 +3,16 @@ import './style.css'
 import playerX from '../../img/playerx.png'
 import playerO from '../../img/playero.png'
 
-const PlayerGame = ({player}) => {
+const PlayerGame = ({ player = false}) => {
    const players = [];
    players['x'] = playerX;
    players['o'] = playerO;
 
-    return <button className="player-game">
-        <img src={players[player]} alt={`Player ${player.toUpperCase()}`} />
+    return (
+    <button className="player-game">
+         {player && <img src={players[player]} alt={`Player ${player.toUpperCase()}`} />}
     </button>
-
+    )
 };
 
 export default PlayerGame
